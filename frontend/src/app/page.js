25ccdc6908f49a70c235c20c749f6a63f3673c91
Mael Galite <mgalite@google.com>
2024-02-11 20:54:16 +0000

    restaurants might not work out of the box

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,8 +2,13 @@ export const dynamic = 'force-dynamic'
 import { getRestaurants } from '@/lib/strapi'
 
 export default async function Home() {
-  const restaurants = await getRestaurants()
-  console.log(restaurants)
+  let restaurants = []
+
+  try {
+    restaurants = await getRestaurants()
+  } catch (error) {
+    console.error(error)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
