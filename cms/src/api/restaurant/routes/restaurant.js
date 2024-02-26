daacf4d051215fb9313572940b230a84edd3348a
Mael Galite <mgalite@google.com>
2024-02-26 17:38:18 +0000

    Set permissions for restaurants on boot

--- cms/src/api/restaurant/routes/restaurant.js
+++ cms/src/api/restaurant/routes/restaurant.js
@@ -1,9 +1,32 @@
-'use strict';
+'use strict'
 
 /**
  * restaurant router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require('@strapi/strapi').factories
 
-module.exports = createCoreRouter('api::restaurant.restaurant');
+module.exports = createCoreRouter('api::restaurant.restaurant', {
+  config: {
+    find: {
+      // @ts-ignore
+      roles: ['authenticated', 'public'],
+    },
+    create: {
+      // @ts-ignore
+      roles: ['authenticated'],
+    },
+    findOne: {
+      // @ts-ignore
+      roles: ['authenticated', 'public'],
+    },
+    update: {
+      // @ts-ignore
+      roles: ['authenticated'],
+    },
+    delete: {
+      // @ts-ignore
+      roles: ['authenticated'],
+    },
+  },
+})
